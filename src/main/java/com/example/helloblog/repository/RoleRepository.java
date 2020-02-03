5d945a0b9fbec8b0f7dad8e0cf3344030a0b0b3d
Meatball <mainmeatball1@gmail.com>
2020-02-03 17:31:46 +0300

    Create User and Role Repositories

---
+++ src/main/java/com/example/helloblog/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.helloblog.repository;
+
+import com.example.helloblog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByName(String name);
+}
