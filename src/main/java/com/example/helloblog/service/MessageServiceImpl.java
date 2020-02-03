a163d895f8fb3f6835b56496fbc166105b76b5df
Meatball <mainmeatball1@gmail.com>
2020-02-04 00:57:50 +0300

    Update save method in MessageService

--- src/main/java/com/example/helloblog/service/MessageServiceImpl.java
+++ src/main/java/com/example/helloblog/service/MessageServiceImpl.java
@@ -2,8 +2,12 @@ package com.example.helloblog.service;
 
 import com.example.helloblog.entity.Message;
 import com.example.helloblog.repository.MessageRepository;
+import com.example.helloblog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,10 +16,14 @@ import java.util.Optional;
 public class MessageServiceImpl implements MessageService {
 
     private MessageRepository messageRepository;
+    private UserRepository userRepository;
+    private UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    public MessageServiceImpl(MessageRepository messageRepository) {
+    public MessageServiceImpl(MessageRepository messageRepository, UserRepository userRepository, UserDetailsServiceImpl userDetailsService) {
         this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
+        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -27,13 +35,18 @@ public class MessageServiceImpl implements MessageService {
     public Message findById(int id) {
         Optional<Message> message = messageRepository.findById(id);
         if (!message.isPresent()) {
-            throw new RuntimeException("Did not find message id - " + id);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Message with id " + id + " is not found.");
         }
         return message.get();
     }
 
     @Override
     public void save(Message message) {
+        UserDetails user = userDetailsService.getCurrentUser();
+        if (user == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User is unauthorized.");
+        }
+        message.setUser(userRepository.findByUsername(user.getUsername()));
         messageRepository.save(message);
     }
 
