c99ea21f97a148431aff7ac404f58ea1d5df9ee6
Meatball <mainmeatball1@gmail.com>
2020-02-03 03:58:10 +0300

    Create User and Role entities

---
+++ src/main/java/com/example/helloblog/entity/Role.java
@@ -0,0 +1,49 @@
+package com.example.helloblog.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int id;
+
+    @Column(name = "role_name")
+    private String name;
+
+    @OneToMany(mappedBy = "role")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/example/helloblog/entity/User.java
@@ -0,0 +1,59 @@
+package com.example.helloblog.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(unique = true)
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name="role_id")
+    private Role role;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
