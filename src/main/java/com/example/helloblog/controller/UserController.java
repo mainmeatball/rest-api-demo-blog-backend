b7552c41a6f1b1725df090a991b8772c75b50ad6
Meatball <mainmeatball1@gmail.com>
2020-02-04 00:48:57 +0300

    Update UserController to get logged person for test

--- src/main/java/com/example/helloblog/controller/UserController.java
+++ src/main/java/com/example/helloblog/controller/UserController.java
@@ -3,16 +3,20 @@ package com.example.helloblog.controller;
 import com.example.helloblog.dto.SignUpDto;
 import com.example.helloblog.entity.User;
 import com.example.helloblog.service.SecurityService;
+import com.example.helloblog.service.UserDetailsServiceImpl;
 import com.example.helloblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class UserController {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     private UserService userService;
     private SecurityService securityService;
@@ -28,5 +32,11 @@ public class UserController {
         User user = userService.signUp(signUpDto);
         securityService.autoLogin(signUpDto.getUsername(), signUpDto.getPassword());
         return user;
+    }
+
+    // just for a test check which user is logged in
+    @GetMapping("/who_am_i")
+    public UserDetails whoAmI() {
+        return userDetailsService.getCurrentUser();
     }
 }
