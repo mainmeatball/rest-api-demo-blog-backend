925159c41cac4329f2037d23f48275fd089b0390
Meatball <mainmeatball1@gmail.com>
2020-02-06 00:48:11 +0300

    Fix UserController using another policy

--- src/main/java/com/example/helloblog/controller/UserController.java
+++ src/main/java/com/example/helloblog/controller/UserController.java
@@ -2,8 +2,6 @@ package com.example.helloblog.controller;
 
 import com.example.helloblog.dto.UserDto;
 import com.example.helloblog.entity.User;
-import com.example.helloblog.service.SecurityService;
-import com.example.helloblog.service.UserDetailsServiceImpl;
 import com.example.helloblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,22 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class UserController {
 
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
-
     private UserService userService;
-    private SecurityService securityService;
 
     @Autowired
-    public UserController(UserService userService, SecurityService securityService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.securityService = securityService;
     }
 
+
+    // TODO: implement autologin feature
     @PostMapping("/sign_up")
     public User signUp(@RequestBody UserDto userDto) {
-        User user = userService.save(userDto);
-        securityService.autoLogin(userDto.getUsername(), userDto.getPassword());
-        return user;
+        return userService.save(userDto);
     }
 }
