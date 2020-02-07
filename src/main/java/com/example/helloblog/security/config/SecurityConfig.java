c2b5f77aac899459564a737e5f091b48a8fbd268
Meatball <mainmeatball1@gmail.com>
2020-02-07 03:46:19 +0300

    Add exceptionHandling in case of unauthorized access

--- src/main/java/com/example/helloblog/security/config/SecurityConfig.java
+++ src/main/java/com/example/helloblog/security/config/SecurityConfig.java
@@ -1,13 +1,14 @@
-package com.example.helloblog.config;
+package com.example.helloblog.security.config;
 
-import com.example.helloblog.config.jwt.JwtConfigurer;
-import com.example.helloblog.config.jwt.TokenProvider;
+import com.example.helloblog.security.UnauthorizedEntryPoint;
+import com.example.helloblog.security.config.jwt.JwtConfigurer;
+import com.example.helloblog.security.config.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,29 +20,35 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 @EnableConfigurationProperties
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private UserDetailsService userDetailsService;
     private TokenProvider tokenProvider;
+    private UnauthorizedEntryPoint unauthorizedEntryPoint;
 
     @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService, TokenProvider tokenProvider) {
+    public SecurityConfig(UserDetailsService userDetailsService, TokenProvider tokenProvider, UnauthorizedEntryPoint unauthorizedEntryPoint) {
         this.userDetailsService = userDetailsService;
         this.tokenProvider = tokenProvider;
+        this.unauthorizedEntryPoint = unauthorizedEntryPoint;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
             .csrf().disable()
             .authorizeRequests()
                 .antMatchers("/login", "/sign_up").permitAll()
-                .antMatchers("/", "/messages**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                .antMatchers("/**").hasAuthority("ROLE_ADMIN")
+                .antMatchers("/", "/messages/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
+                .and()
+            .exceptionHandling()
+                .authenticationEntryPoint(unauthorizedEntryPoint)
                 .and()
             .apply(securityConfigurerAdapter());
     }
