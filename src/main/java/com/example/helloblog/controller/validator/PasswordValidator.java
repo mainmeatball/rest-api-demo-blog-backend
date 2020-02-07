91767fb6f32125ec9ae3752b757ed946a998857e
Meatball <mainmeatball1@gmail.com>
2020-02-07 03:29:42 +0300

    Create PasswordValidator for AuthenticationController

---
+++ src/main/java/com/example/helloblog/controller/validator/PasswordValidator.java
@@ -0,0 +1,21 @@
+package com.example.helloblog.controller.validator;
+
+import org.springframework.stereotype.Service;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Service
+public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        return password != null
+                && !password.trim().isEmpty()
+                && password.matches("^[0-9A-Za-z_]+$");
+    }
+}
---
+++ src/main/java/com/example/helloblog/controller/validator/ValidPassword.java
@@ -0,0 +1,15 @@
+package com.example.helloblog.controller.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+@Documented
+public @interface ValidPassword {
+    String message() default "Invalid password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
