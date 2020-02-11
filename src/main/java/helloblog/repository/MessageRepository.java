e4bd45f4e21389d2128b7042e9c9d7ebae81eb36
Meatball <mainmeatball1@gmail.com>
2020-02-11 16:12:27 +0300

    Update PagingAndSortingRepository to JpaRepository

--- src/main/java/helloblog/repository/MessageRepository.java
+++ src/main/java/helloblog/repository/MessageRepository.java
@@ -1,13 +1,14 @@
 package helloblog.repository;
 
 import helloblog.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MessageRepository extends PagingAndSortingRepository<Message, Integer> {
+public interface MessageRepository extends JpaRepository<Message, Integer> {
 
     List<Message> findByUser_Username(String username);
 }
