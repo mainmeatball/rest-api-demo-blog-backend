26bd5f3ead3b113af9940e2c917041d122db4447
Meatball <mainmeatball1@gmail.com>
2020-02-19 23:16:03 +0300

    Add tags field to Message

--- src/main/java/helloblog/entity/Message.java
+++ src/main/java/helloblog/entity/Message.java
@@ -1,11 +1,10 @@
 package helloblog.entity;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-
-// TODO: add tags
 
 @Entity(name="messages")
 public class Message {
@@ -31,53 +30,70 @@ public class Message {
     @NotNull
     private User user;
 
+    @ManyToMany
+    @JoinTable(
+            name = "messages_tags",
+            joinColumns = @JoinColumn(name = "message_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags;
+
     public Message() {
     }
 
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public LocalDateTime getLocalDateTime() {
         return localDateTime;
     }
+
     public void setLocalDateTime(LocalDateTime localDateTime) {
         this.localDateTime = localDateTime;
     }
+
     public String getContent() {
         return content;
     }
+
     public void setContent(String text) {
         this.content = text;
     }
+
     public int getLikes() {
         return likes;
     }
+
     public void setLikes(int likes) {
         this.likes = likes;
     }
+
     public User getUser() {
         return user;
     }
+
     public void setUser(User user) {
         this.user = user;
     }
+
     public void upvote() {
         this.likes++;
     }
+
     public void downvote() {
         this.likes--;
     }
 
-    @Override
-    public String toString() {
-        return "Message{" +
-                "id=" + id +
-                ", localDateTime=" + localDateTime +
-                ", content='" + content + '\'' +
-                ", likes=" + likes +
-                '}';
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
     }
 }
