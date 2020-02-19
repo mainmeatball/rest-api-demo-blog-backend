9d84986c8ac5ba322830b0f09015822d3c585cec
Meatball <mainmeatball1@gmail.com>
2020-02-19 23:16:29 +0300

    Add TagRepository

---
+++ src/main/java/helloblog/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package helloblog.repository;
+
+import helloblog.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+    Tag findByName(String name);
+}
