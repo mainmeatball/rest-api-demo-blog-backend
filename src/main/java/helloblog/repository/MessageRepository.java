db0a7605f2acb44c958a7457a1e09479b3f4ae83
Meatball <mainmeatball1@gmail.com>
2020-02-24 18:32:52 +0300

    Rename MessageRepository

--- src/main/java/helloblog/repository/MessageRepository.java
+++ src/main/java/helloblog/repository/MessageRepository.java
@@ -3,5 +3,5 @@ package helloblog.repository;
 import helloblog.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MessagesRepository extends JpaRepository<Message, Integer>, MessageRepositoryCustom {
+public interface MessageRepository extends JpaRepository<Message, Integer>, MessageRepositoryCustom {
 }
